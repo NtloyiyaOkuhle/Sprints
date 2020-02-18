c1c72ba0b6674e1134818deb07eb2abb72554300
Okuhle <blessing.ntloyiya@gmail.com>
2020-02-18 12:48:25 +0200

    Add files via upload

---
+++ Product1.sql
@@ -0,0 +1,48 @@
+Chips	Quantity	Selling-Prices(Rands)	Stock-price(Rands)
+Simba	10	16.2	14
+Lays	10	18	15
+Doritoes	10	15	13
+Pringles	10	21	18
+Stylos	10	1	5.5
+			
+Cooldrinks	Quantity	Selling-Prices(Rands)	Stock-price(Rands)
+Coke	10	20.2	15
+Fanta	10	20	15
+Stoney	10	20	15
+Sprite	10	20	15
+Sparlet	10	13	10
+			
+Quantity	Selling-Prices(Rands)	Stock-price(Rands)	
+Card bury	10	12.2	10
+Tex	10	17	15
+Dairy Milk	10	30	25
+Bubbles	10	25	20
+P.S	10	10	8
+			
+Pies	Quantity	Selling-Prices	Stock-price
+Pepper Steak	8	20.2	15
+Chicken	10	17	15
+Mutton	6	30	25
+Beef	9	25	20
+Steck7Kidney	10	15	10
+			
+Quantity(Boxes)	Selling-Prices	Stock-price	
+Banana	6	70	50
+Apple	6	60	50
+Orange	6	50	30
+Pear	6	100	80
+Grapes	6	120	100
+			
+Quantity(Boxes)	Selling-Prices	Stock-price	
+Vannila	24	8	5
+Chocolate	24	8	5
+Mint-oreo	24	8	5
+Blue-berry	24	8	5
+Rasberry	24	8	5
+			
+Quantity(Bunch)	Selling-Prices	Stock-price	
+Spinach	8	10	8
+Cabbage	8	15	10
+Tomato	8	10	8
+Potatoes	8	20	10
+Pumpkin	8	30	20
